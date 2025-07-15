2936a41ad96299e81d2b376c957a506a8b6b9843
Hiel Saraiva <hielsaraiva11.hs@gmail.com>
2025-07-14 22:03:05 -0300

    feat: implementando método detectar deadlock

--- src/main/java/br/com/ifce/so/projectdeadlockdetection/models/GerenciadorRecursos.java
+++ src/main/java/br/com/ifce/so/projectdeadlockdetection/models/GerenciadorRecursos.java
@@ -1,6 +1,8 @@
 package br.com.ifce.so.projectdeadlockdetection.models;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.Semaphore;
 
 public class GerenciadorRecursos {
@@ -28,18 +30,91 @@ public class GerenciadorRecursos {
         R = new int[10][10];
     }
 
-    public void verificarDeadlock() {
+    public ArrayList<Integer> verificarDeadlock() {
         synchronized (mutex) {
-            
+
+            // Identificar processos ativos (que têm alocação ou requisição)
+            boolean[] processoAtivo = new boolean[10];
+            for (int i = 0; i < 10; i++) {
+                processoAtivo[i] = false;
+                for (int j = 0; j < 10; j++) {
+                    if (recursos[j] != null && (C[i][j] > 0 || R[i][j] > 0)) {
+                        processoAtivo[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            // Encontre o vetor de recursos disponíveis A
+            int[] Work = new int[10]; // Cópia de A para simulação
+            for (int i = 0; i < 10; i++) {
+                if (recursos[i] != null) {
+                    Work[i] = A[i].availablePermits();
+                } else {
+                    Work[i] = 0;
+                }
+            }
+
+            // Vetor para marcar processos que podem terminar
+            boolean[] Finish = new boolean[10];
+            for (int i = 0; i < 10; i++) {
+                Finish[i] = !processoAtivo[i]; // Processos inativos já estão "terminados"
+            }
+
+            // Algoritmo principal de detecção
+            boolean mudanca = true;
+            while (mudanca) {
+                mudanca = false;
+                for (int i = 0; i < 10; i++) {
+                    if (processoAtivo[i] && !Finish[i]) {
+                        boolean podeExecutar = true;
+                        for (int j = 0; j < 10; j++) {
+                            if (recursos[j] != null && R[i][j] > Work[j]) {
+                                podeExecutar = false;
+                                break;
+                            }
+                        }
+                        if (podeExecutar) {
+                            for (int j = 0; j < 10; j++) {
+                                if (recursos[j] != null) {
+                                    Work[j] += C[i][j];
+                                }
+                            }
+                            Finish[i] = true;
+                            mudanca = true;
+                        }
+                    }
+                }
+            }
+
+            // Identificar processos em deadlock (apenas entre os ativos)
+            ArrayList<Integer> processosEmDeadlock = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                if (processoAtivo[i] && !Finish[i]) {
+                    processosEmDeadlock.add(i + 1); // IDs são de 1 a 10
+                }
+            }
+
+            // Garantindo ordenação por id
+            Collections.sort(processosEmDeadlock);
+
+            // Log
+            if (!processosEmDeadlock.isEmpty()) {
+                System.out.println("DEADLOCK DETECTADO!");
+                System.out.println("Processos em deadlock: " + processosEmDeadlock);
+            } else {
+                System.out.println("Sistema livre de deadlock");
+            }
+
+            return processosEmDeadlock;
         }
     }
 
-    public Recurso solicitarRecurso(Integer processoId) { // <- AQUI
+    public Recurso solicitarRecurso(Integer processoId) {
         int indiceRecurso = -1;
         Recurso recursoSolicitado = null;
 
         synchronized (mutex) {
-
             int recursosValidos = 0;
             for (int i = 0; i < recursos.length; i++) {
                 if (recursos[i] != null) {
@@ -66,7 +141,26 @@ public class GerenciadorRecursos {
         }
 
         try {
+            // Verifica se a thread foi interrompida antes de bloquear
+            if (Thread.currentThread().isInterrupted()) {
+                synchronized (mutex) {
+                    R[processoId - 1][indiceRecurso]--;
+                }
+                return null;
+            }
+
             A[indiceRecurso].acquire();
+
+            // Verifica se a thread foi interrompida após adquirir o recurso
+            if (Thread.currentThread().isInterrupted()) {
+                synchronized (mutex) {
+                    R[processoId - 1][indiceRecurso]--;
+                    C[processoId - 1][indiceRecurso]++;
+                }
+                // Libera imediatamente o recurso
+                A[indiceRecurso].release();
+                return null;
+            }
 
             synchronized (mutex) {
                 R[processoId - 1][indiceRecurso]--;
@@ -76,6 +170,13 @@ public class GerenciadorRecursos {
 
             return recursoSolicitado;
 
+        } catch (InterruptedException e) {
+            // Se for interrompido durante o acquire, desfaz a requisição
+            synchronized (mutex) {
+                R[processoId - 1][indiceRecurso]--;
+            }
+            Thread.currentThread().interrupt(); // Mantém o status de interrupção
+            return null;
         } catch (Exception e) {
             synchronized (mutex) {
                 R[processoId - 1][indiceRecurso]--;
@@ -100,6 +201,22 @@ public class GerenciadorRecursos {
                 C[processoId - 1][indiceRecurso]--;
                 System.out.println("Processo " + processoId + " liberou recurso " + recurso.getNome());
                 A[indiceRecurso].release();
+            }
+        }
+    }
+
+    public void zerarRequisicoes(int idProcesso) {
+        synchronized (mutex) {
+            for (int j = 0; j < 10; j++) {
+                R[idProcesso - 1][j] = 0;
+            }
+        }
+    }
+
+    public void zerarAlocacoes(int idProcesso) {
+        synchronized (mutex) {
+            for (int j = 0; j < 10; j++) {
+                C[idProcesso - 1][j] = 0;
             }
         }
     }
